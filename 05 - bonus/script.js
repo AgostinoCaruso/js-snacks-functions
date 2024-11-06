af0d798210603667db0e618f2f67bfbacc33138c
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-06 17:54:34 +0100

    5 finish

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,20 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+const today = new Date();
+const prendoOre = today.getHours();
+function Saluta(stringa){
+    if(prendoOre > 6 && prendoOre <=13)
+        return `Buongiorno ${stringa}`; 
+    else if(prendoOre >13 && prendoOre <=17){
+        return `Buon pomeriggio ${stringa}`;
+    }else{
+        return `Buonasera ${stringa}`;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const salutiamo = Saluta(name);
+console.log(salutiamo);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
